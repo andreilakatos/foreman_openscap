971cf3e16886f06d407c6cff8a21ed3b6a5e0ee5
MariaAga <mariaaga@redhat.com>
2026-07-21 15:29:16 +0100

    Fixes #39549 - clean up unused js code

--- webpack/components/IndexTable/IndexTableHelper.js
+++
@@ -1,6 +0,0 @@
-import { addSearch } from '../../helpers/pageParamsHelper';
-
-export const refreshPage = (history, params = {}) => {
-  const url = addSearch(history.location.pathname, params);
-  history.push(url);
-};
--- webpack/helpers/tableHelper.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { TableText } from '@patternfly/react-table';
-
-export const linkCell = (path, text) => (
-  <TableText>
-    <Link to={path}>{text}</Link>
-  </TableText>
-);
